64be3eb88811adadce0fec47f1907cae516b8256
docwali777 <drgauvin@mgmcare.org>
2017-04-15 16:23:26 -0400

    figuring out IP address with npm-ip and req.ip

--- server.js
+++ server.js
@@ -2,17 +2,30 @@ const express = require('express');
 const app = express();
 const ejs = require('ejs')
 const PORT = process.env.PORT || 3000
-const ip = require('ip')
+const ip = require('ip');
+var getIP = require('external-ip')();
+
+var publicIpAddress ='';
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res)=>{
   const ipAddress = ip.address()
 const ip1 = req.ip
-  res.render('index', {
-ipAddress,
-  ip1
-  })
+
+const IP = {
+  privateIpAddress: `IPv6 ${req.ip} or can be written as ${ipAddress}`,
+
+}
+getIP(function (err, ip) {
+  if (err) {
+      // every service in the list has failed
+      throw err;
+  }
+IP.PublicIpAddress = ip
+  res.json(IP);
+});
+
 })
 
 
