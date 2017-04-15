a21bacbe5d20f7b46f3b1ce5daeb130843a8b145
docwali777 <drgauvin@mgmcare.org>
2017-04-15 17:04:28 -0400

    figuring out IP address with npm-ip and req.ip

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const ejs = require('ejs')
 const PORT = process.env.PORT || 3000
 const ip = require('ip');
 var extIP = require('external-ip');
+var get_ip = require('ipware')().get_ip;
+
+ app.use(function(req, res, next) {
+    //  var ip_info = get_ip(req);
+     var ip_info = get_ip(req, right_most_proxy='True')
+     res.json(ip_info);
+     // { clientIp: '127.0.0.1', clientIpRoutable: false }
+     next();
+ });
 
 var getIP = extIP({
     replace: true,
@@ -16,28 +25,28 @@ var publicIpAddress ='';
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res)=>{
-  const ipAddress = ip.address()
-const ip1 = req.ip
-
-const IP = {
-  privateIpAddress: `${ipAddress}`,
-
-}
-// res.render('index', {
-//   ipAddress,
-//   ip1
+// app.get('/', (req, res)=>{
+//   const ipAddress = ip.address()
+// const ip1 = req.ip
+//
+// const IP = {
+//   privateIpAddress: `${ipAddress}`,
+//
+// }
+// // res.render('index', {
+// //   ipAddress,
+// //   ip1
+// // })
+// getIP(function (err, ip) {
+//   if (err) {
+//       // every service in the list has failed
+//       throw err;
+//   }
+// IP.PublicIpAddress = ip
+//   res.json(IP);
+// });
+//
 // })
-getIP(function (err, ip) {
-  if (err) {
-      // every service in the list has failed
-      throw err;
-  }
-IP.PublicIpAddress = ip
-  res.json(IP);
-});
-
-})
 
 
 
